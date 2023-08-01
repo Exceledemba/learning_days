860a10c4b41ec1f93da687c34851767ac9611433
Exceledemba <edembauche@gmail.com>
2023-08-01 06:55:51 +0000

    boolean expression

---
+++ boolean/0-boo.c
@@ -0,0 +1,14 @@
+// boolean
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+
+{
+	bool value1 = true;
+	bool value2 = false;
+
+	printf("value1= %d\n", value1);
+	printf("value2= %d\n", value2);
+	return 0;
+}
