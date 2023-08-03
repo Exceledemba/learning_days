a8050f2521e3fdfbb09205ba46ae3d750194972c
Exceledemba <edembauche@gmail.com>
2023-08-03 00:35:27 +0000

    relearning for loop

---
+++ for-loop/1-loo.c
@@ -0,0 +1,74 @@
+//for loop
+//sum of numbers from 1 to 100
+
+#include <stdio.h>
+
+int main()
+
+{
+
+	int sum = 0;
+
+	int i;
+
+	for(int i = 1; i <= 100; i++);
+
+	{
+
+		int sum = sum + i;
+
+	}
+
+		printf("%d\n", sum);
+
+	return 0;
+
+}
+
+/*
+// sum of even numbers
+
+#include <stdio.h>
+
+int main()
+
+{
+
+	int sum = 0;
+
+	for(i = 2; i < 101; i = i + 2)
+
+	{
+
+		sum = sum + i;
+
+	}
+		printf("Sum of even numbers 1 to 100=%d\n", sum);
+
+	return 0;
+
+}
+
+
+//sum of odd numbers
+
+#include <stdio.h>
+
+int main()
+
+{
+
+	int sum = 0;
+	for(i = 1; i < 101; i = i + 2);
+
+	{
+
+		sum = sum + i;
+
+	}
+
+		printf(" Sum of odd numbers 1 to 100=%d\n", sum);
+	
+	return 0;
+
+}*/
---
+++ for-loop/README.md
@@ -0,0 +1,16 @@
+For Loop
+syntax;
+for (initializationExpression; testExpression; updateExpression)
+	{
+		//code inside the loop;
+	}
+initializationExpression; it declares and initialises a variable.
+example;
+int i = 0; and it is executed only once.
+testExpression; it's a boolean expression that evaluate to true or false.
+example;
+i < 5; if the testexpression is true the code in the for run runs.
+updateExpression; changes the value of the variable.
+example
+i++ or i--
+
